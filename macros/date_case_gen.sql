88b9d586594a5908c1e09bb5e0a38cfd804b1bec
David Hrncir <david.hrncir@fivetran.com>
2025-09-09 18:50:45 -0600

    Update date_case_gen.sql

--- macros/date_case_gen.sql
+++ macros/date_case_gen.sql
@@ -2,9 +2,9 @@
 CASE when {{col_name}} is null or {{col_name}} in (' ', '') 
 or substring({{col_name}},0,1) in(0,3,4,5,6,7,8,9) 
 THEN NULL ELSE 
-{%- if target.type == 'databricks' -%}TO_DATE({{col_name}}, 'yyyyMMdd'
-{%- elif target.type == 'bigquery' -%}PARSE_DATE('%Y%m%d', {{col_name}}
-{%- else -%}TO_DATE({{col_name}}, 'YYYYMMDD'
+{%- if target.type == 'databricks' -%} TO_DATE({{col_name}}, 'yyyyMMdd'
+{%- elif target.type == 'bigquery' -%} PARSE_DATE('%Y%m%d', {{col_name}}
+{%- else -%} TO_DATE({{col_name}}, 'YYYYMMDD'
 {%- endif -%}
 ) END 
 as {{alias_name}}
